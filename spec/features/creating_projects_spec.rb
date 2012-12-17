71b66410c0c57beade1cb360a3359e23fd861132
Alper Sunar <asunar.git@gmail.com>
2012-12-17 17:12:19 -0600

    First controller with passing specs

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe 'Creating projects' do
   it 'welcomes the userdd' do
     visit '/'
-    page.should have_content('Welcome')
+    page.should have_content('New Project')
+    click_link('New Project')
+    fill_in('Name', :with => 'A brand new project')
+    click_on('Create Project')
+    page.should have_content('created')
+    page.should have_content('A brand new project')
   end
 end
 
